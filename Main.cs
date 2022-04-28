6999d2af208070e01c84cd3d41c7dfe31d4269f8
Mezque <31026406+Mezque@users.noreply.github.com>
2022-04-28 20:49:26 -0300

    updapte 1.1.0.0

--- Main.cs
+++ Main.cs
@@ -8,7 +8,9 @@ namespace MicDotRecolour
 {
     internal class Main : MelonMod
     {
-        public static bool RGBToggle = false;
+        internal static bool RGBToggle = false;
+        internal static Image ImageDot = GameObject.Find("UserInterface").transform.Find("UnscaledUI/HudContent/Hud/VoiceDotParent/VoiceDotDisabled").GetComponent<Image>();
+        internal static GameObject UserInterface = GameObject.Find("UserInterface");
 
         public override void OnApplicationStart()
         {
@@ -24,17 +26,14 @@ namespace MicDotRecolour
 
         internal static IEnumerator StartUserInterfaceInitIEnumerator()
         {
-            while (GameObject.Find("UserInterface") == null) yield return null;
+            while (UserInterface == null) yield return null;
             Modules.ModLog.Msg(ConsoleColor.Yellow, "[Info] User Interface Found! Starting Mic Dot Recolour!");
             Icon();
         }
 
         public static void Icon()
         {
-            if (Modules.Prefs.MicRgb.Value == true)
-            {
-            }
-            else
+            if (!Modules.Prefs.MicRgb.Value)
             {
                 GameObject.Find("UserInterface").transform.Find("UnscaledUI/HudContent/Hud/VoiceDotParent/VoiceDotDisabled").GetComponent<Image>().color = new Color(float.Parse(Modules.Prefs.MicColourR.Value) / 255f, float.Parse(Modules.Prefs.MicColourG.Value) / 255f, float.Parse(Modules.Prefs.MicColourB.Value) / 255f, float.Parse(Modules.Prefs.MicColourA.Value) / 255f);
                 Modules.ModLog.Msg(ConsoleColor.Yellow, "[Info] Mic Dot Has Been Recoloured!");
@@ -43,10 +42,10 @@ namespace MicDotRecolour
 
         public override void OnUpdate()
         {
-            while (GameObject.Find("UserInterface") == null) return;
+            while (UserInterface == null) return;
             Color Rainbow = new Color((float)Math.Sin(2 * Time.time * 0.5f + 0.5f), (float)Math.Sin((2 * Time.time) + Math.PI * 2.0 / 3.0) * 0.5f + 0.5f, (float)Math.Sin((2 * Time.time) + Math.PI * 4.0 / 3.0) * 0.5f + 0.5f);
-            if (!Modules.Prefs.MicRgb.Value == true) return;
-            GameObject.Find("UserInterface").transform.Find("UnscaledUI/HudContent/Hud/VoiceDotParent/VoiceDotDisabled").GetComponent<Image>().color = Rainbow;
+            if (Modules.Prefs.MicRgb.Value) return;
+            ImageDot.color = Rainbow;
         }
     }
 }
