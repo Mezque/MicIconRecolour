a11f370d70bb70b0dd673d118f48404ee713f87a
Mezque <31026406+Mezque@users.noreply.github.com>
2022-05-12 19:31:27 -0300

    removed unused instanced logger for error or warning

--- Modules/ModLog.cs
+++ Modules/ModLog.cs
@@ -7,7 +7,5 @@ namespace MicDotRecolour.Modules
     {
         internal static MelonLogger.Instance Conlog = new("MicDotRecolour");
         internal static void Msg(ConsoleColor ConColour, string obj) => Conlog.Msg(ConColour, obj);
-        internal static void Error(string obj) => Conlog.Error(obj);
-        internal static void Warning(string obj) => Conlog.Warning(obj);
     }
 }
