bb6a83b2ce456e825aa0c119391f4b39ba826e85
Mezque <31026406+Mezque@users.noreply.github.com>
2022-05-12 18:59:14 -0300

    Redid how prefs work to do them properly?

--- Modules/Prefs.cs
+++ Modules/Prefs.cs
@@ -4,22 +4,12 @@ namespace MicDotRecolour.Modules
 {
     internal class Prefs
     {
-        internal static void InitPrefs()
-        {
-            MDR = MelonPreferences.CreateCategory("Mic Dot Recolour");
-            MicColourR = MDR.CreateEntry("Mic Colour R", "215.628", "Mic Icon (Red 0-255)  (Default 215.628)");
-            MicColourG = MDR.CreateEntry("Mic Colour G", "0", "Mic Icon (Green; 0-255) (Default 0)");
-            MicColourB = MDR.CreateEntry("Mic Colour B", "0", "Mic Icon (Blue; 0-255) (Default 0)");
-            MicColourA = MDR.CreateEntry("Mic Colour A", "155.805", "Mic Icon (Alpha; 0-255) (Default 155.805)");
-            MicRgb = MDR.CreateEntry<bool>("Mic RGB", false, "Toggle for rgb mode");
-            MelonLoader.MelonPreferences.Save();
-        }
-
-        internal static MelonPreferences_Category MDR;
-        internal static MelonPreferences_Entry<string> MicColourR;
-        internal static MelonPreferences_Entry<string> MicColourG;
-        internal static MelonPreferences_Entry<string> MicColourB;
-        internal static MelonPreferences_Entry<string> MicColourA;
-        internal static MelonPreferences_Entry<bool> MicRgb;
+        private static readonly MelonPreferences_Category MDR = MelonPreferences.CreateCategory("Mic Dot Recolour");
+        internal static MelonPreferences_Entry<string> MicColourR = MDR.CreateEntry("Mic Colour R", "215.628", "Mic Icon (Red 0-255)  (Default 215.628)");
+        internal static MelonPreferences_Entry<string> MicColourG = MDR.CreateEntry("Mic Colour G", "0", "Mic Icon (Green; 0-255) (Default 0)");
+        internal static MelonPreferences_Entry<string> MicColourB = MDR.CreateEntry("Mic Colour B", "0", "Mic Icon (Blue; 0-255) (Default 0)");
+        internal static MelonPreferences_Entry<string> MicColourA = MDR.CreateEntry("Mic Colour A", "155.805", "Mic Icon (Alpha; 0-255) (Default 155.805)");
+        internal static MelonPreferences_Entry<float> Scale = MDR.CreateEntry("Mic Dot Scale", 1f, "Mic Dot Scale (Recommended 0.5-2.1");
+        internal static MelonPreferences_Entry<bool> MicRgb = MDR.CreateEntry("Mic RGB", false, "Toggle for rgb mode");
     }
 }
