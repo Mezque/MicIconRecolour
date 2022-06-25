96993bbef06dd40baaaa4fb6295e00b669c3190d
Mezque <31026406+Mezque@users.noreply.github.com>
2022-06-25 17:22:35 -0300

    Added the cs file to add available update notices (next release)

---
+++ Melonloader/Modules/UpdateNotice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicDotRecolour.Modules
+{
+    internal class UpdateNotice
+    {
+        internal static void UpdateCheck()
+        {
+
+        }
+    }
+}
