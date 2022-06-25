787f45c4997c8a86a7e0296bbca5582624aee1c7
Mezque <31026406+Mezque@users.noreply.github.com>
2022-06-25 17:03:14 -0300

    update build number before push

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,10 +11,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("Mezque")]
 [assembly: AssemblyCulture("")]
-[assembly: MelonInfo(typeof(MicDotRecolour.Main), "MicDotRecolour", "1.2.0.0", "Mezque")]
+[assembly: MelonInfo(typeof(MicDotRecolour.Main), "MicDotRecolour", $"{MicDotRecolourVerion.Version}", "Mezque", "https://github.com/Mezque/MicDotRecolour/releases/latest/download/MicDotRecolour.dll")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonColor(ConsoleColor.Blue)]
 [assembly: ComVisible(false)]
 [assembly: Guid("ed4799fc-d7a8-4ab0-b6b8-4cd1583397c7")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")] 
+[assembly: AssemblyVersion($"{MicDotRecolourVerion.Version}")]
+[assembly: AssemblyFileVersion($"{MicDotRecolourVerion.Version}")]
+internal struct MicDotRecolourVerion
+{
+    internal const string Version = "1.2.1.0";
+}
