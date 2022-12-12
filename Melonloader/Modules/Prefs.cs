c84f67630f9cae628e6c3d1ad13d9df4a50d61d9
Micah <31026406+Mezque@users.noreply.github.com>
2022-12-12 05:06:56 -0400

    Set RGB to true by default for now due to a lack of UIX or simular mods existing to change melon prefs in game atm.

--- Melonloader/Modules/Prefs.cs
+++ Melonloader/Modules/Prefs.cs
@@ -10,7 +10,7 @@ namespace MicDotRecolour.Modules
         internal static MelonPreferences_Entry<string> MicColourB = MDR.CreateEntry("Mic Colour B", "0", "Mic Icon (Blue; 0-255) (Default 0)");
         internal static MelonPreferences_Entry<string> MicColourA = MDR.CreateEntry("Mic Colour A", "155.805", "Mic Icon (Alpha; 0-255) (Default 155.805)");
         internal static MelonPreferences_Entry<float> Scale = MDR.CreateEntry("Mic Dot Scale", 1f, "Mic Dot Scale (Recommended 0.5-2.1");
-        internal static MelonPreferences_Entry<bool> MicRgb = MDR.CreateEntry("Mic RGB", false, "Toggle for rgb mode");
+        internal static MelonPreferences_Entry<bool> MicRgb = MDR.CreateEntry("Mic RGB", true, "Toggle for rgb mode"); //going to set rgb to true for now due to a lack of UIX or simular mods to configure melon prefs in game atm.
         internal static MelonPreferences_Entry<bool> ToggleMuteIconVisable = MDR.CreateEntry("Mute Icon Visability", true, "Toggle the visability of the mute icon on or off");
         internal static MelonPreferences_Entry<bool> ToggleSpeakIconVisable = MDR.CreateEntry("Speaking Icon Visability", true, "Toggle the visability of the speaking/talking icon on or off");
     }
