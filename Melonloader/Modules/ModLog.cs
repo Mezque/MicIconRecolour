6f4e4d06722213c9e7273656666e4914155cb669
Micah <31026406+Mezque@users.noreply.github.com>
2022-12-12 05:14:35 -0400

    Changed logger instance to MicIconRecolour from MicDotRecolour

--- Melonloader/Modules/ModLog.cs
+++ Melonloader/Modules/ModLog.cs
@@ -5,7 +5,7 @@ namespace MicIconRecolour.Modules
 {
     internal static class ModLog
     {
-        internal static MelonLogger.Instance Conlog = new("MicDotRecolour");
+        internal static MelonLogger.Instance Conlog = new("MicIconRecolour");
         internal static void Msg(ConsoleColor ConColour, string obj) => Conlog.Msg(ConColour, obj);
         internal static void Warn(string obj) => Conlog.Warning(obj);
     }
